0879e9d3dde2daabe266bc59e37979394b07756c
YonisHussein <yonismhussein@gmail.com>
2022-07-25 21:20:55 -0400

    updated readme

--- 02-Challenge/Assets/js/script.js
+++ 02-Challenge/Assets/js/script.js
@@ -1,15 +1,15 @@
 var buttonEl = document.querySelector('.button');
-var humidityEl = document.querySelector('.humidityEl');
-var windSpeedEl = document.querySelector('windSpeed');
+var humidityEl = document.querySelector('.humidity');
+var windSpeedEl = document.querySelector('.windSpeed');
 var inputValueEl = document.querySelector('.inputvalue');
 var cityEl = document.querySelector('.city');
 var timeEl = document.querySelector('.time');
 var tempEl = document.querySelector('.temp');
-var descEl = document.querySelector('desc');
+var descEl = document.querySelector('.desc');
 
 buttonEl.addEventListener('click', function (){
 
-    fetch('https://api.openweathermap.org/data/2.5/forecast>q=' + inputValueEl.value + '&cnt=168&appid=ef67e5009d4692986dc5a35ad88f7290')
+    fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + inputValueEl.value + '&cnt=168&appid=ef67e5009d4692986dc5a35ad88f7290')
     .then(response => response.json())
     .then(data => {
         var cityElValue = data['city']['name']
@@ -17,7 +17,7 @@ buttonEl.addEventListener('click', function (){
         var tempElValue = data['list'][0]['main']['temp']
         var desElValue = data['list'][0]['weather'][0]['description']
         var humidityElValue = data['list'][0]['main']['humidity']
-        var windSpeedValue = data['list'][0]['wind']['speed']
+        var windSpeedElValue = data['list'][0]['wind']['speed']
 
         cityEl.innerHTML = cityElValue
         timeEl.innerHTML = 'Time: ' + tempElValue
